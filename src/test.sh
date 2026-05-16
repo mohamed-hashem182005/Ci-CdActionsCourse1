1c7d4ce9259bc4cfe03e741da6fb60ecb4af433a
Mohashem <jamm7198@gmail.com>
2026-05-17 01:19:47 +0300

    update node js app2

--- src/test.sh
+++ src/test.sh
@@ -1,17 +1,13 @@
 EXPECTED="hello, Test"
 
-OUTPUT=$(node -e "console.log(require('./src/app')('Test'))" )
+OUTPUT=$(node -e "process.stdout.write(require('./src/app')('Test'))")
 
-if [ "$OUTPUT" == "$EXPECTED" ]; then
-
-echo "test passed!"
-
-exit 0
-
-else 
-
-echo "test failed !"
-
-exit 1
-
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+  echo "test passed!"
+  exit 0
+else
+  echo "test failed!"
+  echo "Expected: $EXPECTED"
+  echo "Got: $OUTPUT"
+  exit 1
 fi
